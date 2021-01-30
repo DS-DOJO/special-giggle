10ad687920767027cf4a19252267b0c9a4fe058f
Rahul Pandey <rpandey1901@gmail.com>
2021-01-30 19:07:02 +0100

    initial commit

---
+++ VSCodeTest/First Step.py
@@ -0,0 +1,11 @@
+import math
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+x=10
+print (sys.version)
+print(sys.executable)
+print(pd.DataFrame(data=[1, 2, 3]).head())
