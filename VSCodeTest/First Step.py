208f3a8d73c9a096c0835f294acdc5c2d094b4ad
Rahul Pandey <rpandey1901@gmail.com>
2021-01-30 19:21:36 +0100

    Check 2

--- VSCodeTest/First Step.py
+++ VSCodeTest/First Step.py
@@ -5,7 +5,8 @@ import sys
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-x=10
-print (sys.version)
+
+x = 10
+print(sys.version)
 print(sys.executable)
 print(pd.DataFrame(data=[1, 2, 3]).head())
